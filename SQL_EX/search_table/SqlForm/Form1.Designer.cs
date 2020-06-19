aa60c4081f098eb1c20e743bd10af94a82308b41
JontCont <information9527@gmail.com>
2020-06-19 15:14:01 +0800

    update 109/06/19

---
+++ SQL_EX/search_table/SqlForm/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace SqlForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.bDP000000BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.mESHZDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.MES_HZDataSet = new SqlForm.MES_HZDataSet();
+            this.bDP00_0000TableAdapter = new SqlForm.MES_HZDataSetTableAdapters.BDP00_0000TableAdapter();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bDP000000BindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mESHZDataSetBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MES_HZDataSet)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 89);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 27;
+            this.dataGridView1.Size = new System.Drawing.Size(751, 268);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // bDP000000BindingSource
+            // 
+            this.bDP000000BindingSource.DataMember = "BDP00_0000";
+            this.bDP000000BindingSource.DataSource = this.mESHZDataSetBindingSource;
+            // 
+            // mESHZDataSetBindingSource
+            // 
+            this.mESHZDataSetBindingSource.DataSource = this.MES_HZDataSet;
+            this.mESHZDataSetBindingSource.Position = 0;
+            // 
+            // MES_HZDataSet
+            // 
+            this.MES_HZDataSet.DataSetName = "MES_HZDataSet";
+            this.MES_HZDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // bDP00_0000TableAdapter
+            // 
+            this.bDP00_0000TableAdapter.ClearBeforeFill = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "資料表";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(96, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 15);
+            this.label2.TabIndex = 5;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(245, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(290, 71);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DataBindings.Add(new System.Windows.Forms.Binding("SelectedValue", this.bDP000000BindingSource, "par_name", true));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(69, 33);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(195, 23);
+            this.comboBox1.TabIndex = 6;
+            this.comboBox1.SelectedValueChanged += new System.EventHandler(this.comboBox1_SelectedValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(775, 365);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bDP000000BindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mESHZDataSetBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MES_HZDataSet)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource mESHZDataSetBindingSource;
+        private MES_HZDataSet MES_HZDataSet;
+        private System.Windows.Forms.BindingSource bDP000000BindingSource;
+        private MES_HZDataSetTableAdapters.BDP00_0000TableAdapter bDP00_0000TableAdapter;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ SQL_EX/search_table/SqlForm/Form1.cs
@@ -0,0 +1,72 @@
+﻿using SqlForm.MES_HZDataSetTableAdapters;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Threading;
+
+namespace SqlForm
+{
+    public partial class Form1 : Form
+    {
+
+        string str, Sqlstr;
+        SqlConnection connection;
+        SqlDataAdapter da;
+        DataSet ds;
+
+        public Form1()
+        {
+            InitializeComponent();
+ //           textBox1.Text = MES_HZDataSet.Tables[3].ToString();
+            int cout = MES_HZDataSet.Tables.Count;
+            //textBox1.Text = cout.ToString();
+            for(int i=0; i < 26; i++)
+            {
+                comboBox1.Items.Add(MES_HZDataSet.Tables[i].ToString());
+            }
+            comboBox1.Text = MES_HZDataSet.Tables[0].ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: 這行程式碼會將資料載入 'mES_HZDataSet.BDP00_0000' 資料表。您可以視需要進行移動或移除。
+            //this.bDP00_0000TableAdapter.Fill(this.MES_HZDataSet.BDP00_0000);
+            str = ConfigurationManager.ConnectionStrings["conndb"].ConnectionString;
+            Sqlstr = "select * from BDP03_0000";
+            connection = new SqlConnection(str);
+
+
+            da = new SqlDataAdapter(Sqlstr, connection);
+            ds = new DataSet();
+            da.Fill(ds);
+            this.dataGridView1.DataSource = ds.Tables[0].DefaultView;
+
+        }
+
+        private void comboBox1_SelectedValueChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                //if (textBox1.Text.Equals("")) textBox1.Text = "BDP03_0000";
+
+                Sqlstr = "select * from " + comboBox1.Text;
+                da = new SqlDataAdapter(Sqlstr, connection);
+
+                ds = new DataSet();
+                da.Fill(ds);
+                this.dataGridView1.DataSource = ds.Tables[0].DefaultView;
+            }
+            catch{}
+        }
+
+
+    }
+}
